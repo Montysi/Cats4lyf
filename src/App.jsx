1c8c62c8d8f946a7768484fee0bc13380062b75f
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-26 12:10:24 +0100

    readded homepage for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,8 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { faker } from "@faker-js/faker";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [cats, setCats] = useState([]);
-  const [errorMsg, setErrorMsg] = useState("");
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://api.thecatapi.com/v1/images/search?limit=10"
-      );
-      if (!response.ok) {
-        throw new Error("...");
-      }
-      const data = await response.json();
-      const catData = data.mp((cat, index) => {
-        return {
-          catID: index,
-          catImage: cat.url,
-          name: faker.name.findName(),
-          breed: faker.animal.cat(),
-          price: faker.finance.amount(150, 600),
-          gender: faker.name.gender(),
-        };
-      });
-      console.log(catData);
-      setCats(catData);
-    } catch (error) {
-      console.log(error);
-      setErrorMsg(error.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <>
       <Routes>

df83d0fa26b8e5a4812de56744ae5d352979dc05
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-06-26 14:14:52 +0100

    added function for the basketModal that displays items

--- src/Components/BasketModal.jsx
+++ src/Components/BasketModal.jsx
@@ -2,6 +2,30 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { BasketContext } from "./context/BasketContext";
 
-constBasketModal =()= {
-    const {showModal, closeModal, basketItems}
+const BasketModal =()=> {
+    const {showModal, closeModal, basketItems} = useContext(BasketContext);
+
+    if (!showModal) {
+        return null;
+    }
+
+    return (
+        <ModalBackground>
+            <ModalContainer>
+                <CloseButton onClick={closeModal}>Close</CloseButton>
+                <h2>ShoppingBasket</h2>
+                <ul>
+                    {basketItems.length === 0 ? (
+                        <li>Your basket is empty</li>
+                    ) : (
+                        basketItems.map ((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))
+                    )}
+                </ul>
+            </ModalContainer>
+        </ModalBackground>
+    )
 }
+
+export default BasketModal

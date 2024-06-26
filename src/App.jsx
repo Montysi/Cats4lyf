45621d5b37f939f3ebcfa29a3a8c05804613603f
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-26 13:25:24 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
+
+
 
 const App = () => {
   return (
-    <>
+   
       <Routes>
         <Route path="/" />
         <Route path="/" />
         <Route path="/" />
       </Routes>
-    </>
+    
   );
 };
 
--- src/Components/CatInfoPage.jsx
+++ src/Components/CatInfoPage.jsx
@@ -1,3 +1,42 @@
+import { faker } from "@faker-js/faker";
+
+const createRandomUser
+ = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+const USERS = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
+
+const createCats = () => {
+    return {
+        name: faker.string.name(),
+        age: faker.number.age(),
+        personality: faker.string.personality(),
+        faveFood: faker.string.faveFood(),
+        price: faker.string.price(),
+
+    }
+}
+
+const cats = faker.helpers.mutliple(createCats, {
+    count: 10,
+});
+
+console.log(cats)
+
+
+
 const CatInfoPage = () => {
   return <h1>Cat Info Page</h1>;
 };

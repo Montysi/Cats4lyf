70f37d5e116a364a82ceb4a67fe50f7ea2a0f057
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-06-27 14:57:53 +0100

    coded checkout page to display items from Basket Modal

--- src/Components/CheckoutPage.jsx
+++ src/Components/CheckoutPage.jsx
@@ -1,5 +1,34 @@
+import React, {useContext} from "react";
+import {BasketContext} from "..Context/BasketContext";
+import styled from "styled-components";
+
 const CheckoutPage = () => {
-  return <h1>Checkout Page</h1>;
+  const {basketItems} = useContext(BasketContext);
+
+  return (
+    <CheckoutContainer>
+      <h2>Checkout</h2>
+      {basketItems.length === 0 ? (
+        <p>Your basket is empty</p>
+      ) : (
+        <ul>
+          {basketItems.map((item, index) => (
+            <li key={index}>
+              {basketItems.name} - £{item.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </CheckoutContainer>
+  )
 };
 
 export default CheckoutPage;
+
+const CheckoutContainer = styled.div`
+  
+`;
+
+const Checkoutitem = styled.li`
+  
+`;
